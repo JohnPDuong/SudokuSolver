a40565cefcf8f266db22076e0110a1b31729f75f
jpduong12 <jpduong12@gmail.com>
2021-01-19 20:15:10 -0800

    Defined find_next_space function

--- solver.py
+++ solver.py
@@ -1,6 +1,12 @@
 #returns the row, col for the next empty space in grid
 #returns None, None if there are no empty spaces
 def find_next_space (puzzle):
+    for row in range(9): #0 to 8
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r, col
+    
+    return None, None
 
 #primary function
 def solve(puzzle):
