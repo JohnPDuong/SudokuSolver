b17af37f1d719e83b6ef82390f21522fcc710f5b
jpduong12 <jpduong12@gmail.com>
2021-01-19 20:09:26 -0800

    Initial commit

---
+++ solver.py
@@ -0,0 +1,7 @@
+#returns the row, col for the next empty space in grid
+#returns None, None if there are no empty spaces
+def find_next_space (puzzle):
+
+#primary function
+def solve(puzzle):
+    row, col = find_next_space (puzzle)
